7f0640715d45fa135fee13e7d960594f5470eef4
rahultesla <rascimat@gmail.com>
2020-06-15 22:08:13 +0530

    objectified

--- solution.py
+++ solution.py
@@ -1,43 +1,56 @@
 import pandas as pd
-def solve(capacity,unit,machine,cost):
-    o = []
-    tot = 0
-    while capacity > 0:
-        ind = cost.index(min(cost))
-        p = capacity//unit[ind]
-        if p == 0:
-            cost = cost[:ind]
-            continue
-        m = machine[ind]
-        o.append((m,p))
-        capacity = capacity % unit[ind]
-        tot += p*cost[ind]*unit[ind]
-        cost = cost[:ind]
-    return o,tot
+
+class Solution():
+    def __init__(self):
+        self.db = pd.read_csv("db.csv")
         
-db = pd.read_csv("db.csv")
-inputs = list(input("input: ").split(" "))
+        self.c = 10
+        
+        self.d = {"Output":[]}
+    def promptinput(self):
+        inputs = list(input("input: ").split(" "))
+        for i in range(len(inputs)):
+            if "Hour" in inputs[i]:
+                t = int(inputs[i-1])
+            elif inputs[i]=="units":
+                c =  int(inputs[i-1])
+        self.c = c * t
+    def solve(self,capacity,unit,machine,cost):
+        o = []
+        tot = 0
+        while capacity > 0:
+            ind = cost.index(min(cost))
+            p = capacity//unit[ind]
+            if p == 0:
+                cost = cost[:ind]
+                continue
+            m = machine[ind]
+            o.append((m,p))
+            capacity = capacity % unit[ind]
+            tot += p*cost[ind]*unit[ind]
+            cost = cost[:ind]
+        return o,tot
+    def findanswer(self):
+        col = self.db.columns
+        for i in range(2,len(col)):
+            di = {}
+            m = []
+            tot = 0
+            x = self.db[col[i]]/self.db[col[0]]
+            cost = x.to_list()
+            unit = self.db[col[0]].to_list()
+            machine = self.db[col[1]].to_list()
+            a,b = Solution.solve(self,self.c,unit,machine,cost)
+            di["region"] = col[i]
+            di["totalcost"] = '$'+str(int(b))
+            di["machines"] = a
+            self.d["Output"].append(di)
+    def output(self):
+        return self.d
+    
 
-for i in range(len(inputs)):
-    if "Hour" in inputs[i]:
-        t = int(inputs[i-1])
-    elif inputs[i]=="units":
-        c =  int(inputs[i-1])
-c *= t
-d = {"Output":[]}
-col = db.columns
-
-for i in range(2,len(col)):
-    di = {}
-    m = []
-    tot = 0
-    x = db[col[i]]/db[col[0]]
-    cost = x.to_list()
-    unit = db[col[0]].to_list()
-    machine = db[col[1]].to_list()
-    a,b = solve(c,unit,machine,cost)
-    di["region"] = col[i]
-    di["totalcost"] = '$'+str(int(b))
-    di["machines"] = a
-    d["Output"].append(di)
+s = Solution()
+s.promptinput()
+s.findanswer()
+print(s.output())
     
